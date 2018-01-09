bfb09838fc24a6e11a49073f802495a5f71feb17
markpen <exiletheworld@gmail.com>
2018-01-09 13:48:06 -0800

    add comments

--- Algorithm/Count Ones in Binary Number/src/CountOnesInBinaryNumber.cpp
+++ Algorithm/Count Ones in Binary Number/src/CountOnesInBinaryNumber.cpp
@@ -10,6 +10,10 @@ CountOnesInBinaryNumber::~CountOnesInBinaryNumber()
     //dtor
 }
 
+/*
+the key of the question is to eliminate the 0s int the tail quickly
+*/
+
 int CountOnesInBinaryNumber::countOnes(int x) {
     int count = 0;
     while(x) {
